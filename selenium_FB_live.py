cc9f4580567afb7f246b331ff2ca3ce8b95487ff
William-Mou <10530026@sssh.tp.edu.tw>
2017-11-05 22:16:47 +0800

    First
    -分頁設定
    -os.path尋找
    -帳密讀取

---
+++ selenium_FB_live.py
@@ -0,0 +1,34 @@
+
+# coding: utf-8
+
+# In[ ]:
+
+from selenium import webdriver
+import time
+
+#環境設定
+chrome_path=input("chrome_path")
+n=int(input("開啟帳號數量"))
+url=input("直播URL")
+
+#填入帳密表單!!
+email_list=[]
+pass_list=[]
+
+#瀏覽器自動化開啟
+web=[]
+for i in range(n):
+    web.append("")
+    web[i] = webdriver.Chrome(chrome_path)
+    web[i].get(url)
+    web[i].set_window_position(0,0) #瀏覽器位置
+    web[i].set_window_size(700,700) #瀏覽器大小
+    email_input = web[i].find_element_by_id("email")
+    email_input.send_keys(email_list[i])
+    pass_input = web[i].find_element_by_id("pass")
+    pass_input.send_keys(pass_list[i])
+    web[i].find_element_by_id("loginbutton").click()
+    web[i].get(url)    
+    
+time.sleep(5)
+
